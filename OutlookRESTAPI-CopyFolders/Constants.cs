86d6135731c64d924beef8537fed848b4f8f31fd
anujab <anujab@ANUJA-SURFACE>
2017-10-03 10:53:11 -0400

    Adding Copy folder logic

---
+++ OutlookRESTAPI-CopyFolders/Constants.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutlookRESTAPI_CopyFolders
+{
+    class Constants
+    {
+        internal const string ClientId = "e4159be4-deb9-473a-9a23-e54c05f1a01e";
+
+        internal const string OutlookEndpoint = "https://outlook.office.com/";
+
+        internal const string OutlookRestApiEndpoint = OutlookEndpoint + "api/v2.0/me/";
+        
+        internal const  string Messages = OutlookRestApiEndpoint + "messages?$top=1";
+        
+        internal static readonly string[] Scopes = new string[] { OutlookEndpoint + "mail.readwrite" };
+
+        internal const string Folders = OutlookRestApiEndpoint +"MailFolders/";
+
+        internal const string Inbox = Folders + "inbox/childfolders";
+
+        internal const string CopyFolder = Folders + "{0}/copy";
+    }
+}
--- OutlookRESTAPI-CopyFolders/Program.cs
+++ OutlookRESTAPI-CopyFolders/Program.cs
@@ -10,6 +10,24 @@ namespace OutlookRESTAPI_CopyFolders
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("This program copies a folder under Inbox to Drafts");
+            Console.WriteLine("Enter the name of the folder to be copied");
+            string copyFolder = Console.ReadLine();
+
+            var operations = new FolderOperations();
+            string id = operations.FindFolder(copyFolder);
+
+            if (string.IsNullOrEmpty(id))
+            {
+                Console.WriteLine("Cannot find the folder : " + copyFolder);
+            }
+
+            bool copySuccess = operations.CopyFolder(id, "drafts");
+
+            Console.WriteLine(copySuccess
+                ? $"Successfully copied over the folder '{copyFolder}' from Inbox to Drafts"
+                : "Folder Copy Failed");
         }
+
     }
 }
